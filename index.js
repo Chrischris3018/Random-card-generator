b8af6c4d03a28670da5defd6cc0c57d5a844147c
Chrischris3018 <chrischrischris3018@yahoo.com>
2021-10-16 00:58:17 +0000

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 function buildACard() {
-    debugger;
-
+    //Shapes and numbers on card
     const suits = ['♦', '♥', '♠', '♣'];
     const cardValueArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'A', 'Q', 'K', 'J'];
+
+    //Card index and value
     const cardIndex = getRandomArbitrary(0, cardValueArray.length - 1);
-    const theCardValue = cardValueAray[cardIndex];
+    const theCardValue = cardValueArray[cardIndex];
+
+    //Suit index and value
     const suitIndex = getRandomArbitrary(0, suits.length - 1);
     const suitValue = suits[suitIndex];
-    const theCardValue = cardValueArray[cardArray];
-    debugger;
 
+    //HTML links
     const suitSlot = document.getElementById('RandomSuit')
     const cardValueSlot = document.getElementById('RandomCardValue')
     
+    suitSlot.InnerHTML = suitValue;
+    cardValueSlot.InnerHTML = theCardValue;
 }
 
+//Math function
 function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
 }
